7975ddc48ee00dcc2fda65cc845027d8f40180f3
atian155 <atianthony@outlook.com>
2024-03-27 12:54:17 +0200

    Added weather cards

---
+++ src/pages/Apps/Weather/components/WeatherCard.jsx
@@ -0,0 +1,15 @@
+const WeatherCard = ({
+  time = null,
+  temperature = 0,
+  apparent_temperature = null,
+  min_temperature = null,
+  max_temperature = null,
+  precipitation = 0,
+  rain = 0,
+  sunrise = null,
+  sunset = null,
+}) => {
+  return <div>WeatherCard</div>;
+};
+
+export default WeatherCard;
--- src/pages/Apps/Weather/index.jsx
+++ src/pages/Apps/Weather/index.jsx
@@ -5,14 +5,24 @@ import "./styles.css";
 import AppTitle from "../components/AppTitle";
 import { apps } from "../../../data/apps";
 
+// Components
+import WeatherCard from "./components/WeatherCard";
+
 const Weather = () => {
   const app = apps.filter((app) => app.path === "weather")[0];
+
+  const API_URL =
+    "https://api.open-meteo.com/v1/forecast?latitude=33.8933&longitude=35.5016&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,weather_code,cloud_cover,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,rain_sum,precipitation_probability_max,wind_speed_10m_max&forecast_days=4";
 
   return (
     <>
       <AppTitle icon={app.icon} color={app.colors.main} title={app.name}>
         {app.description}
       </AppTitle>
+
+      <section className="weather-cards">
+        <WeatherCard />
+      </section>
     </>
   );
 };
