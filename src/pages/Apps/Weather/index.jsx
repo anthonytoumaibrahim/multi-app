394171e677e386cd3806c912848fee4fac88a6f5
atian155 <atianthony@outlook.com>
2024-03-27 21:57:04 +0200

    Updated API Link

--- src/pages/Apps/Weather/index.jsx
+++ src/pages/Apps/Weather/index.jsx
@@ -102,7 +102,7 @@ const Weather = () => {
   });
 
   const API_URL =
-    "https://api.open-meteo.com/v1/forecast?latitude=33.8933&longitude=35.5016&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,weather_code,cloud_cover,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,rain_sum,precipitation_probability_max,wind_speed_10m_max&forecast_days=4";
+    "https://api.open-meteo.com/v1/forecast?latitude=33.8933&longitude=35.5016&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,weather_code,cloud_cover,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,rain_sum,precipitation_probability_max,wind_speed_10m_max&forecast_days=4&timezone=Asia%2FBeirut";
 
   useEffect(() => {
     // Fetch Weather API data on load
